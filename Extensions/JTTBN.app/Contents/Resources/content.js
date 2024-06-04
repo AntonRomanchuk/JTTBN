290539090b3f707e97946faf640663cfcc95546c
AntonRomanchuk <iosradev@gmail.com>
2024-06-04 15:04:09 +0300

    Updated extensions

---
+++ Extensions/JTTBN.app/Contents/Resources/content.js
@@ -0,0 +1,42 @@
+const ticketTitleElement = document.getElementById("summary-val");
+const ticketNumberElement = document.getElementById("key-val");
+
+const transformations = [
+    [/\n/g, "-"],                 // Replace newline characters with hyphens
+    [/[^a-zA-Z0-9\s\-/.]/g, ""],  // Remove non-alphanumeric characters except space, hyphen, slash, and dot
+    [/\s/g, "-"],                 // Replace spaces with hyphens
+    [/\//g, "-"],                 // Replace slashes with hyphens
+    [/:/g, ""],                   // Remove colons
+    [/-+/g, "-"],                 // Replace consecutive hyphens with a single hyphen
+    [/^-+|-+$/g, ""]              // Remove hyphens from the beginning and end
+];
+
+if (ticketTitleElement && ticketNumberElement) {
+    const buttonContainer = document.createElement("div");
+    buttonContainer.style.marginTop = "10px";
+    const createButton = (buttonId, buttonText) => {
+        const ticketNumber = ticketNumberElement.innerText.trim();
+          const ticketTitle = ticketTitleElement.innerText.trim();
+
+        const button = document.createElement("button");
+        button.id = buttonId;
+        button.innerText = buttonText;
+        button.style.marginRight = "5px";
+        button.addEventListener("click", () => {
+            let string = `${ticketNumber} ${ticketTitle}`
+              for (const [regex, replacement] of transformations) {
+                string = string.replace(regex, replacement);
+              }
+
+              const textToCopy = `${buttonId}/${string}`;
+              navigator.clipboard.writeText(textToCopy);
+            });
+        return button;
+  };
+
+  buttonContainer.appendChild(createButton("feature", "Feature"));
+  buttonContainer.appendChild(createButton("bugfix", "Bugfix"));
+  buttonContainer.appendChild(createButton("release", "Release"));
+
+  ticketTitleElement.insertAdjacentElement("afterend", buttonContainer);
+}
